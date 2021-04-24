ec339ef03225f752adec30e09d2e9505ce7dc481
Sopianna Siagian <82864489+Sopianna082@users.noreply.github.com>
2021-04-24 15:44:24 +0700

    Update defaultone.blade.php

--- resources/views/layouts/defaultone.blade.php
+++ resources/views/layouts/defaultone.blade.php
@@ -34,7 +34,7 @@
               <span class="tab"></span> Website ini  bertujuan untuk Mempromosikan Desa dan Pariwisata Desa Lumban Gaol. <br><br>
               </p> 
             </div>
-            
+
             <div class="col-md-6 col-sm-6 col-xs-12">
               <h2>Link Terkait</h2>
               <a href="https://samosirkab.go.id/">Pemerintah Kabupaten Samosir</a><br>
