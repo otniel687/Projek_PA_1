d610023f05b295c2c4dd33d98074d5e995d06fab
Sopianna Siagian <82864489+Sopianna082@users.noreply.github.com>
2021-05-12 23:05:28 +0700

    FIx FOOTER

--- resources/views/pengrajin/index.blade.php
+++ resources/views/pengrajin/index.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.defaulttwo')
 
-@section('title', 'Pengerajin')
+@section('title', 'Pengrajin')
 
 <!--Header-->
     @section('header')
+    <style>
+     .s{
+       font-family: monospace;
+       color: yellow;
+       font-weight: bold;
+     }
+    </style>
     <section id="pengrajin" class="d-flex align-items-center">
       <div class="container p-4 p-md-5 mb-4 text-white rounded" style="color: #fff;  background-size: cover;">
         <div class="col-md-6 px-0">
@@ -12,12 +19,17 @@
             Pembuatan ulos tidak di buat dengan asal asalan, perlu skill yang cukup bagus dan memiliki ketelitian dalam pembuatannya. Maka perlunya pengerajin yang handal
             untuk membuat dalam pembuatan ulos. Beberapa pengerajin yang ada di Desa Suhisuhi dapat membantu anda memilih produk yang tepat untuk anda.
           </p><br>
+          <br>
+          <div class="s">
+          <b>Khusus Pengrajin,<br>
+          Jika ingin bergabung dengan kami, hanya mengisi data yang ada pada tombol dibawah ini.</b>
           <a class="navbar-brand" href="/join">          
-          <button class="btn-danger mt-3" href="/join"><i class="fas fa-hand-point-right"></i> Join Us (<span>specialty craftsmen</span>)</button></a>
+          <button class="btn-danger mt-3" href="/join"><i class="fas fa-hand-point-right"></i> Bergabunglah dengan kami</button></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-       
+          </div>
+
         </div>
       </div>
     </section>
@@ -72,8 +84,7 @@
               </div>
             @endforeach
 
-          </div><!-- /.row -->
-          </center>
+          </div>
         </div>
       </section>
 @endsection
